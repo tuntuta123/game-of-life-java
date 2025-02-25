a40f02bae5c5a8d4f1c7220c414da8a227c81fb9
gjoresk231 <gjoreskaandrea@gmail.com>
2025-02-25 10:41:28 +0100

    Emojis addes

--- src/conway/GridPanel.java
+++ src/conway/GridPanel.java
@@ -37,27 +37,37 @@ public class GridPanel extends JPanel {
      * 
      * @param g Le contexte graphique utilisé pour dessiner sur le panneau.
      */
-    private void drawGrid(Graphics g) {
-        int cellWidth = getWidth() / grid.getWidth();  
-        int cellHeight = getHeight() / grid.getHeight(); 
+private void drawGrid(Graphics g) {
+    int cellWidth = getWidth() / grid.getWidth();  
+    int cellHeight = getHeight() / grid.getHeight(); 
 
-        for (int y = 0; y < grid.getHeight(); y++) {
-            for (int x = 0; x < grid.getWidth(); x++) {
-                Node node = grid.getNode(x, y);
+    Font emojiFont = new Font("Arial", Font.PLAIN, Math.min(cellWidth, cellHeight) - 5);
+    g.setFont(emojiFont);
+
+    for (int y = 0; y < grid.getHeight(); y++) {
+        for (int x = 0; x < grid.getWidth(); x++) {
+            Node node = grid.getNode(x, y);
+            
+            if (node.isAlive()) {
+                g.setColor(Color.RED);  
+                String emoji = "😊";
+                FontMetrics fm = g.getFontMetrics();
+                int emojiWidth = fm.stringWidth(emoji);
+                int emojiHeight = fm.getHeight();
+                int xPosition = x * cellWidth + (cellWidth - emojiWidth) / 2;
+                int yPosition = y * cellHeight + (cellHeight - emojiHeight) / 2 + fm.getAscent();
                 
-                if (node.isAlive()) {
-                    g.setColor(Color.red);
-                    g.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-                } 
-                else {
-                    g.setColor(Color.pink);
-                    g.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-                }
-                
-                g.setColor(Color.BLACK);
-                g.drawRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+                g.drawString(emoji, xPosition, yPosition);
+            } 
+            else {
+                g.setColor(Color.PINK);
+                g.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
             }
+            g.setColor(Color.BLACK);
+            g.drawRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
         }
     }
+}
+
 }
 
