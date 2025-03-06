8f2b25a3a5d12d0d36ab2ba12eda4d07e0a2f2cc
Mila Bucevska <bucevsk231@C304L-210A17.campus.unicaen.fr>
2025-03-06 12:20:28 +0100

    NodeTest not finished

--- src/conway/tests/NodeTest.java
+++ src/conway/tests/NodeTest.java
@@ -2,87 +2,68 @@ package conway.tests;
 
 import conway.logic.Node;
 
+
+// Je dois ajout le tests pour les corners et les borders 
+
 public class NodeTest {
 
     public NodeTest() {}
 
     public static void runTests() {
-        System.out.println("\n Running tests - Node");
+        System.out.println("\n Runing tests - Node");
 
-
+// test pour le fonction isAlive()
         Node node = new Node(true);  
         assert node.isAlive() : "Node should be alive!";
-        System.out.println("Node initialization with 'alive' state passed");
+        System.out.println("[ isAlive() ] passed");
 
-
+// test pour le fonction setAlive
         node.setAlive(false); 
         assert !node.isAlive() : "Node should be dead after setting alive state to false!";
-        System.out.println("setAlive() and isAlive() tests passed");
+        System.out.println("[ setAlive() ] passed");
+
+// tests pour les voisin d'un cellule
+		  Node north = new Node(true);
+        node.setNorth(north);
+    	  assert node.getNorth() == north : "setNorth() or getNorth() failed! The node's north neighbor should be 'north'.";
+        System.out.println("[setNorth() and getNorth()] passed");
+
+		  Node west = new Node(false);
+        node.setWest(west);
+    	  assert node.getWest() == west : "setWest() or getWest() failed! The node's west neighbor should be 'west'.";
+        System.out.println("[setWest() and getWest()] passed");
+
+		  Node east = new Node(true);
+        node.setEast(east);
+    	  assert node.getEast() == east : "setEast() or getEast() failed! The node's east neighbor should be 'east'.";
+        System.out.println("[setEast() and getEast()] passed");
+
+	     Node south = new Node(true);
+        node.setSouth(south);
+    	  assert node.getSouth() == south : "setSouth() or getSouth() failed! The node's south neighbor should be 'south'.";
+        System.out.println("[setSouth() and getSouth()] passed");
+
+		  Node northEast = new Node(false);
+        node.setNorthEast(northEast);
+    	  assert node.getNorthEast() == northEast : "setNorthEast() or getNorthEast() failed! The node's northEast neighbor should be 'northEast'.";
+        System.out.println("[setNorthEast() and getNorthEast()] passed");
+
+		  Node northWest = new Node(true);
+        node.setNorthWest(northWest);
+    	  assert node.getNorthWest() == northWest : "setNorthWest() or getNorthWest() failed! The node's northWest neighbor should be 'northWest'.";
+        System.out.println("[setNorthWest() and getNorthWest()] passed");
+
+		  Node southEast = new Node(false);
+        node.setSouthEast(southEast);
+    	  assert node.getSouthEast() == southEast : "setSouthEast() or getSouthEast() failed! The node's southEast neighbor should be 'southEast'.";
+        System.out.println("[setSouthEast() and getSouthEast()] passed");
+
+		  Node southWest = new Node(true);
+        node.setSouthWest(southWest);
+    	  assert node.getSouthWest() == southWest : "setSouthWest() or getSouthWest() failed! The node's southWest neighbor should be 'southWest'.";
+        System.out.println("[setSouthWest() and getSouthWest()] passed");
 
 
-        Node northNode = new Node(false);
-        Node southNode = new Node(true);
-        node.setNorth(northNode);
-        node.setSouth(southNode);
-
-        assert node.getNorth() == northNode : "getNorth() failed!";
-        assert node.getSouth() == southNode : "getSouth() failed!";
-        System.out.println("setNorth() and setSouth() tests passed");
-
-
-        Node northEastNode = new Node(true);
-        Node southWestNode = new Node(false);
-        node.setNorthEast(northEastNode);
-        node.setSouthWest(southWestNode);
-
-        assert node.getNorthEast() == northEastNode : "getNorthEast() failed!";
-        assert node.getSouthWest() == southWestNode : "getSouthWest() failed!";
-        System.out.println("Diagonal neighbors tests passed");
-
-
-        Node westNode = new Node(true);
-        node.setWest(westNode);
-        assert node.getWest() == westNode : "getWest() failed!";
-        System.out.println("getWest() test passed");
-
-
-        Node cornerNode = new Node(false);
-        cornerNode.setNorth(null);  
-        cornerNode.setWest(null);  
-        assert cornerNode.getNorth() == null : "(0,0) should not have a north neighbor!";
-        assert cornerNode.getWest() == null : "(0,0) should not have a west neighbor!";
-        System.out.println("Corner node neighbor test passed");
-
-
-        Node center = new Node(true);
-        Node north = new Node(false);
-        Node south = new Node(true);
-        Node east = new Node(false);
-        Node west = new Node(true);
-        Node northEast = new Node(false);
-        Node northWest = new Node(true);
-        Node southEast = new Node(false);
-        Node southWest = new Node(true);
-
-        center.setNorth(north);
-        center.setSouth(south);
-        center.setEast(east);
-        center.setWest(west);
-        center.setNorthEast(northEast);
-        center.setNorthWest(northWest);
-        center.setSouthEast(southEast);
-        center.setSouthWest(southWest);
-
-        assert center.getNorth() == north : "North direction test failed!";
-        assert center.getSouth() == south : "South direction test failed!";
-        assert center.getEast() == east : "East direction test failed!";
-        assert center.getWest() == west : "West direction test failed!";
-        assert center.getNorthEast() == northEast : "NorthEast direction test failed!";
-        assert center.getNorthWest() == northWest : "NorthWest direction test failed!";
-        assert center.getSouthEast() == southEast : "SouthEast direction test failed!";
-        assert center.getSouthWest() == southWest : "SouthWest direction test failed!";
-
-        System.out.println("All directional tests passed!");
     }
 
 }
